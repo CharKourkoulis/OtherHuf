72cf9cd24a0ab0c1106b24d05797ac35adfd674b
CharKourkoulis <cult_bob@hotmail.com>
2020-07-14 21:11:39 +0300

    jwt Authentication Token

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [StringLength(15, MinimumLength=5, ErrorMessage="Password must be longer than 5 characters.")]
         public string Password { get; set; }
     }
 }
