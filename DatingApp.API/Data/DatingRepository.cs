040f4181fa45943cf2fdacb34c69b88b5937c0a8
CharKourkoulis <cult_bob@hotmail.com>
2020-08-18 15:18:29 +0300

    Added API Pagination

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Helpers;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -22,27 +23,31 @@ namespace DatingApp.API.Data
 
         public void Delete<T>(T entity) where T : class
         {
-           _context.Remove(entity);
+            _context.Remove(entity);
         }
 
         public async Task<Photo> GetMainPhotoForUser(int userId) =>
             await _context.Photos.Where(u => u.Id == userId).FirstOrDefaultAsync(p => p.IsMain);
-    
+
 
         public async Task<Photo> GetPhoto(int id) =>
             await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
-       
+
         public async Task<User> GetUser(int id) =>
              await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
 
-        
 
-        public async Task<IEnumerable<User>> GetUsers() =>
-           await  _context.Users.Include(p => p.Photos).ToListAsync();
-     
+
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
+        {
+            var users = _context.Users.Include(p => p.Photos);
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
+        }
+
+
 
         public async Task<bool> SaveAll() =>
-            await _context.SaveChangesAsync() > 0 ;
+            await _context.SaveChangesAsync() > 0;
 
     }
 }
